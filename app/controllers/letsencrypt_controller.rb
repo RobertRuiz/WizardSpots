2dfe0bbe74d0650a192d3895bbac3c43548f1f73
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-09-29 13:22:43 -0400

    ssl certificate programmed, revisions to homepage, etc.

--- app/controllers/letsencrypt_controller.rb
+++ app/controllers/letsencrypt_controller.rb
@@ -1,5 +1,5 @@
 class LetsencryptController < ApplicationController
   def challenge
-    render text: "FTwISoMIw8YH6LntRaPoDiki-F1Mt9kRXlF_r_r1Lnk.2YjvTA8nDg9TKT95JrSPwHmyOJXBmCMf3YbeNNYLkOs"
+    render text: ""
   end
 end
